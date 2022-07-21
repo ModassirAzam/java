c882bc641a2f1944735bfef4464b6242df737d69
modassir10099 <109756200+modassir10099@users.noreply.github.com>
2022-07-21 23:07:51 +0530

    Add files via upload

---
+++ basics/Sum.java
@@ -0,0 +1,19 @@
+package package_modassir;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value of A :");
+		int a = sc.nextInt();
+		System.out.println("Enter the value of B :");
+		int b = sc.nextInt();
+		int sum = a +b;
+		System.out.println("The sum of a and b is :"+sum);
+		
+	}
+
+}
