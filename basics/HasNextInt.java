e724a4c6f72babd70cbc09e0eae7d9cf3199a686
modassir10099 <109756200+modassir10099@users.noreply.github.com>
2022-07-22 14:18:08 +0530

    Add files via upload

---
+++ basics/HasNextInt.java
@@ -0,0 +1,18 @@
+package package_modassir;
+
+import java.util.Scanner;
+
+public class HasNextInt {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.hasNextInt());
+
+	}
+	
+}
