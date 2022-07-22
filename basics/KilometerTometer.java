c75f9fa23ec671c3a55ca391eeefd328e7c0f8f5
modassir10099 <109756200+modassir10099@users.noreply.github.com>
2022-07-22 14:11:17 +0530

    Add files via upload

---
+++ basics/KilometerTometer.java
@@ -0,0 +1,21 @@
+package package_modassir;
+
+import java.util.Scanner;
+
+public class KilometerTometer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float meter;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the distance in kilometer ");
+		float km = sc.nextFloat();
+		meter = km*1000;
+		System.out.println("The distance in meter is "+meter);
+		
+		
+		
+
+	}
+
+}
