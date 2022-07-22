a20bf462e2617bacfb2dbccdb00e21aa505da1f7
modassir10099 <109756200+modassir10099@users.noreply.github.com>
2022-07-22 14:03:18 +0530

    Add files via upload

---
+++ basics/UserGreeting.java
@@ -0,0 +1,16 @@
+package package_modassir;
+import java.util.Scanner;
+
+public class UserGreeting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter your name : ");
+		String name = s.next();
+		System.out.println("Hello " +name+ " have a nice day");
+		
+
+	}
+
+}
