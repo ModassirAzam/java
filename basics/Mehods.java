a4b34ff4239a588b1ef3198870bf5a98ba342771
modassir10099 <109756200+modassir10099@users.noreply.github.com>
2022-07-27 13:25:31 +0530

    Add files via upload
    Methods in java

---
+++ basics/Mehods.java
@@ -0,0 +1,28 @@
+package package_modassir;
+import java.util.Scanner;
+
+public class Mehods {
+	
+	static int sum(int a, int b) {
+		int c;
+		c=a+b;
+		return c;
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mehods sc= new Mehods();
+		int b=sc.sum(9,1);
+		System.out.println(b);
+		//System.out.println("Enter the value of a :");
+		///int x = sc.nextInt();
+		//System.out.println("Enter the value of b :");
+		//int y = sc.nextInt();
+		int v=sum(6,7);
+		System.out.println(v);
+
+	}
+
+}
